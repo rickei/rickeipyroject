0813e49a782b4a55089e7705e181bd9641589969
rickei <rickei88@gmail.com>
2015-02-11 00:13:41 +0800

    t

--- checkextip.py
+++ checkextip.py
@@ -1,8 +1,16 @@
 
+#! /usr/bin/env python
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
 
 ## python3 version
 
-import urllib.request
+#import urllib.request
 import re
 
 print("we will try to open this url, in order to get IP Address")
@@ -11,9 +19,10 @@ url = "http://checkip.dyndns.org"
 
 print(url)
 
-request = urllib.request.urlopen(url).read().decode('utf-8')
+request = urlopen(url).read().decode('utf-8')
 
-regxip = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}')
+
+regxip = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}")
 
 
 theIP = re.findall(regxip, request)
